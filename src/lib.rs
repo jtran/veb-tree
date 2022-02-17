a0c83c679742d48beb3938c39b40ae3978a83914
Jonathan Tran <jonnytran@gmail.com>
2022-02-17 02:26:36 -0500

    Move tests to separate file

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,9 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::mem::{replace, swap};
 
+#[cfg(test)]
+mod tests;
+
 #[derive(Debug, Clone)]
 pub struct VanEmdeBoasTree<K, V> {
     min: Option<(K, V)>,
@@ -371,79 +374,3 @@ impl_van_emde_boas_key!(u32);
 impl_van_emde_boas_key!(u64);
 impl_van_emde_boas_key!(u128);
 impl_van_emde_boas_key!(usize);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn keys() {
-        assert_eq!(u32::MAX.cluster_size(), 1u32 << 16);
-        assert_eq!(u64::MAX.cluster_size(), 1u64 << 32);
-        assert_eq!(u128::MAX.cluster_size(), 1u128 << 64);
-    }
-
-    #[test]
-    fn is_empty() {
-        let mut t = VanEmdeBoasTree::<u32, u32>::new();
-        assert_eq!(t.is_empty(), true);
-        t.insert(1, 10);
-        assert_eq!(t.is_empty(), false);
-        t.remove(&1);
-        assert_eq!(t.is_empty(), true);
-    }
-
-    #[test]
-    fn insert_same_key_overwrites() {
-        let mut t = VanEmdeBoasTree::<u32, u32>::new();
-        assert_eq!(t.insert(1, 10), None);
-        assert_eq!(t.successor(&0), Some((1, 10)));
-        // Return the old value.
-        assert_eq!(t.insert(1, 30), Some(10));
-        assert_eq!(t.successor(&0), Some((1, 30)));
-    }
-
-    #[test]
-    fn insert_successor() {
-        let mut t = VanEmdeBoasTree::<u32, u32>::new();
-        t.insert(1, 10);
-        assert_eq!(t.successor(&0), Some((1, 10)));
-        t.insert(3, 30);
-        assert_eq!(t.successor(&0), Some((1, 10)));
-        assert_eq!(t.successor(&2), Some((3, 30)));
-    }
-
-    #[test]
-    fn insert_predecessor() {
-        let mut t = VanEmdeBoasTree::<u32, u32>::new();
-        t.insert(3, 30);
-        assert_eq!(t.predecessor(&4), Some((3, 30)));
-        t.insert(1, 10);
-        assert_eq!(t.predecessor(&4), Some((3, 30)));
-        assert_eq!(t.predecessor(&2), Some((1, 10)));
-    }
-
-    #[test]
-    fn insert_remove_successor() {
-        let mut t = VanEmdeBoasTree::<u32, u32>::new();
-        t.insert(1, 10);
-        t.remove(&1);
-        assert_eq!(t.successor(&0), None);
-    }
-
-    #[test]
-    fn successor_when_not_in_cluster() {
-        let mut t = VanEmdeBoasTree::<u32, u32>::new();
-        t.insert(1, 10);
-        t.insert(u32::MAX, 30);
-        assert_eq!(t.successor(&2), Some((u32::MAX, 30)));
-    }
-
-    #[test]
-    fn predecessor_when_not_in_cluster() {
-        let mut t = VanEmdeBoasTree::<u32, u32>::new();
-        t.insert(1, 10);
-        t.insert(u32::MAX, 30);
-        assert_eq!(t.predecessor(&u32::MAX), Some((1, 10)));
-    }
-}
---
+++ src/tests.rs
@@ -0,0 +1,72 @@
+use super::*;
+
+#[test]
+fn keys() {
+    assert_eq!(u32::MAX.cluster_size(), 1u32 << 16);
+    assert_eq!(u64::MAX.cluster_size(), 1u64 << 32);
+    assert_eq!(u128::MAX.cluster_size(), 1u128 << 64);
+}
+
+#[test]
+fn is_empty() {
+    let mut t = VanEmdeBoasTree::<u32, u32>::new();
+    assert_eq!(t.is_empty(), true);
+    t.insert(1, 10);
+    assert_eq!(t.is_empty(), false);
+    t.remove(&1);
+    assert_eq!(t.is_empty(), true);
+}
+
+#[test]
+fn insert_same_key_overwrites() {
+    let mut t = VanEmdeBoasTree::<u32, u32>::new();
+    assert_eq!(t.insert(1, 10), None);
+    assert_eq!(t.successor(&0), Some((1, 10)));
+    // Return the old value.
+    assert_eq!(t.insert(1, 30), Some(10));
+    assert_eq!(t.successor(&0), Some((1, 30)));
+}
+
+#[test]
+fn insert_successor() {
+    let mut t = VanEmdeBoasTree::<u32, u32>::new();
+    t.insert(1, 10);
+    assert_eq!(t.successor(&0), Some((1, 10)));
+    t.insert(3, 30);
+    assert_eq!(t.successor(&0), Some((1, 10)));
+    assert_eq!(t.successor(&2), Some((3, 30)));
+}
+
+#[test]
+fn insert_predecessor() {
+    let mut t = VanEmdeBoasTree::<u32, u32>::new();
+    t.insert(3, 30);
+    assert_eq!(t.predecessor(&4), Some((3, 30)));
+    t.insert(1, 10);
+    assert_eq!(t.predecessor(&4), Some((3, 30)));
+    assert_eq!(t.predecessor(&2), Some((1, 10)));
+}
+
+#[test]
+fn insert_remove_successor() {
+    let mut t = VanEmdeBoasTree::<u32, u32>::new();
+    t.insert(1, 10);
+    t.remove(&1);
+    assert_eq!(t.successor(&0), None);
+}
+
+#[test]
+fn successor_when_not_in_cluster() {
+    let mut t = VanEmdeBoasTree::<u32, u32>::new();
+    t.insert(1, 10);
+    t.insert(u32::MAX, 30);
+    assert_eq!(t.successor(&2), Some((u32::MAX, 30)));
+}
+
+#[test]
+fn predecessor_when_not_in_cluster() {
+    let mut t = VanEmdeBoasTree::<u32, u32>::new();
+    t.insert(1, 10);
+    t.insert(u32::MAX, 30);
+    assert_eq!(t.predecessor(&u32::MAX), Some((1, 10)));
+}
