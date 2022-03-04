a1122e4c10b026b18f61d13a7bb78ef40ad088b6
Jonathan Tran <jonnytran@gmail.com>
2022-03-04 00:42:20 -0500

    Remove duplicate benchmark code by using a macro

--- benches/successor.rs
+++ benches/successor.rs
@@ -51,136 +51,89 @@ fn bench_successor_single(c: &mut Criterion) {
     group.finish();
 }
 
-fn bench_successor_multiple_in_order(c: &mut Criterion) {
-    let mut group = c.benchmark_group("successor_multiple_in_order");
-    group.measurement_time(Duration::from_secs(20));
-    let mut rng = rand::thread_rng();
+macro_rules! bench_successor_key {
+    ($fn_name: ident, $name: expr, $sort_keys: expr) => {
+        fn $fn_name(c: &mut Criterion) {
+            let mut group = c.benchmark_group($name);
+            group.measurement_time(Duration::from_secs(20));
+            let mut rng = rand::thread_rng();
 
-    for num_keys in [
-        10_000, 100_000, 500_000, 1_000_000, 10_000_000, 20_000_000,
-        30_000_000, 40_000_000,
-    ] {
-        let num_targets = 100_00;
+            for num_keys in [
+                10_000, 100_000, 500_000, 1_000_000, 10_000_000, 20_000_000,
+                30_000_000, 40_000_000,
+            ] {
+                let num_targets = 100_00;
 
-        if num_keys >= 500_000 {
-            // Reduce sample size for larger inputs.
-            group.sample_size(50);
+                if num_keys >= 500_000 {
+                    // Reduce sample size for larger inputs.
+                    group.sample_size(50);
+                }
+
+                // Generate random keys.
+                let keys: Vec<u64> = (0..num_keys).collect();
+
+                // Insert the same keys into each implementation.
+                let mut tree = veb_tree::VebTreeMap::<u64, u64>::new();
+                for k in &keys {
+                    tree.insert(*k, *k);
+                }
+
+                let mut b_tree: BTreeMap<u64, u64> = BTreeMap::new();
+                for k in &keys {
+                    b_tree.insert(*k, *k);
+                }
+
+                // Generate random keys to search for.
+                let mut target_keys: Vec<u64> = (0..num_targets)
+                    .map(|_| rng.gen_range(0..=u64::MAX))
+                    .collect();
+
+                if $sort_keys {
+                    // Look them up in sorted order.
+                    target_keys.sort_unstable();
+                }
+
+                // Benchmark each implementation.
+                group.bench_with_input(
+                    BenchmarkId::new("VebTreeMap", num_keys),
+                    &num_keys,
+                    |b, _i| {
+                        b.iter(|| {
+                            for target in &target_keys {
+                                tree.successor(target);
+                            }
+                        })
+                    },
+                );
+
+                group.bench_with_input(
+                    BenchmarkId::new("BTreeMap", num_keys),
+                    &num_keys,
+                    |b, _i| {
+                        b.iter(|| {
+                            for target in &target_keys {
+                                b_tree.range(target..).next();
+                            }
+                        })
+                    },
+                );
+            }
+            group.finish();
         }
-
-        // Generate random keys.
-        let keys: Vec<u64> =
-            (0..num_keys).map(|_| rng.gen_range(0..=u64::MAX)).collect();
-
-        // Insert the same keys into each implementation.
-        let mut tree = veb_tree::VebTreeMap::<u64, u64>::new();
-        for k in &keys {
-            tree.insert(*k, *k);
-        }
-
-        let mut b_tree: BTreeMap<u64, u64> = BTreeMap::new();
-        for k in &keys {
-            b_tree.insert(*k, *k);
-        }
-
-        // Generate random keys to search for.
-        let mut target_keys: Vec<u64> = (0..num_targets)
-            .map(|_| rng.gen_range(0..=u64::MAX))
-            .collect();
-
-        // Look them up in sorted order.
-        target_keys.sort_unstable();
-
-        // Benchmark each implementation.
-        group.bench_with_input(
-            BenchmarkId::new("VebTreeMap", num_keys),
-            &num_keys,
-            |b, _i| {
-                b.iter(|| {
-                    for target in &target_keys {
-                        tree.successor(target);
-                    }
-                })
-            },
-        );
-
-        group.bench_with_input(
-            BenchmarkId::new("BTreeMap", num_keys),
-            &num_keys,
-            |b, _i| {
-                b.iter(|| {
-                    for target in &target_keys {
-                        b_tree.range(target..).next();
-                    }
-                })
-            },
-        );
-    }
-    group.finish();
+    };
 }
 
-fn bench_successor_multiple_random_order(c: &mut Criterion) {
-    let mut group = c.benchmark_group("successor_multiple_random_order");
-    group.measurement_time(Duration::from_secs(20));
-    let mut rng = rand::thread_rng();
+bench_successor_key!(
+    bench_successor_multiple_in_order,
+    "successor_multiple_in_order",
+    true
+);
 
-    for num_keys in [
-        10_000, 100_000, 500_000, 1_000_000, 10_000_000, 20_000_000,
-        30_000_000, 40_000_000,
-    ] {
-        let num_targets = 100_00;
-
-        if num_keys >= 500_000 {
-            // Reduce sample size for larger inputs.
-            group.sample_size(50);
-        }
-
-        // Generate random keys.
-        let keys: Vec<u64> =
-            (0..num_keys).map(|_| rng.gen_range(0..=u64::MAX)).collect();
-
-        // Insert the same keys into each implementation.
-        let mut tree = veb_tree::VebTreeMap::<u64, u64>::new();
-        for k in &keys {
-            tree.insert(*k, *k);
-        }
-
-        let mut b_tree: BTreeMap<u64, u64> = BTreeMap::new();
-        for k in &keys {
-            b_tree.insert(*k, *k);
-        }
-
-        // Generate random keys to search for.
-        let target_keys: Vec<u64> = (0..num_targets)
-            .map(|_| rng.gen_range(0..=u64::MAX))
-            .collect();
-
-        // Benchmark each implementation.
-        group.bench_with_input(
-            BenchmarkId::new("VebTreeMap", num_keys),
-            &num_keys,
-            |b, _i| {
-                b.iter(|| {
-                    for target in &target_keys {
-                        tree.successor(target);
-                    }
-                })
-            },
-        );
-
-        group.bench_with_input(
-            BenchmarkId::new("BTreeMap", num_keys),
-            &num_keys,
-            |b, _i| {
-                b.iter(|| {
-                    for target in &target_keys {
-                        b_tree.range(target..).next();
-                    }
-                })
-            },
-        );
-    }
-    group.finish();
-}
+bench_successor_key!(
+    bench_successor_multiple_random_order,
+    "successor_multiple_random_order",
+    false
+);
 
 criterion_group!(
     benches,
